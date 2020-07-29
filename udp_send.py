4b8b5d3d0a8636f5a5fbc92506c57d60b717bf8d
Skrat0401 <robin.vogelhaus@googlemail.com>
2020-07-29 20:20:03 +0200

    added bytebuffer

--- udp_send.py
+++ udp_send.py
@@ -1,5 +1,10 @@
 import socket
-   
+
+size = 8
+list_pkt = []
+
+packet = bytearray(size)
+
 UDP_IP = "84.184.253.96"
 UDP_PORT = 5000
 MESSAGE = b"Thomas es funktioniert du bist mein HELD!!"
@@ -7,6 +12,16 @@ MESSAGE = b"Thomas es funktioniert du bist mein HELD!!"
 print("UDP target IP: %s" % UDP_IP)
 print("UDP target port: %s" % UDP_PORT)
 print("message: %s" % MESSAGE)
+print("Please enter your command: ")
+commandobyte = input()
+print("Please enter your sequenznumber: ")
+sequenz = input()
+print("Please enter the adress to read or write: ")
+adr = input()
+print(f"{packet}")
+list_pkt = commandobyte
+
+
 sock = socket.socket(socket.AF_INET, # Internet
                        socket.SOCK_DGRAM) # UDP
 sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
