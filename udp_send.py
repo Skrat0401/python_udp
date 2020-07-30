a0d128f06b101a9a5df98a81ef949249e726d019
Skrat0401 <robin.vogelhaus@googlemail.com>
2020-07-30 12:58:24 +0200

    dynamic bytearray

--- udp_send.py
+++ udp_send.py
@@ -1,32 +1,21 @@
 import socket
-import struct
 
-size = 8
-list_pkt = []
-ii = 1
-packet = bytearray([0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
 
-#"84.184.253.96"
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5000
-MESSAGE = b"Thomas es funktioniert du bist mein HELD!!"
 
-print("UDP target IP: %s" % UDP_IP)
-print("UDP target port: %s" % UDP_PORT)
-print("message: %s" % MESSAGE)
+packet_description = ["cmd", "seq_no", "adr0", "adr1", "adr2", "adr3", "finish"]
+packet = bytearray([0x00 for _ in packet_description])
 
-print("Please enter your command (#1, #2, #3, #4): ")
-packet[0] = int(input())
-print("Please enter your sequenznumber: ")
-packet[1] = int(input())
+for ii, description in enumerate(packet_description):
+  while True:
+    try:
+      packet[ii] = int(input(f"Please enter your {description}:"))
+      break
+    except ValueError:
+      print("Input out of range!")
 
-while ii < 5:
-  print(f" {ii} : Please enter the adress to read or write: ")
-  packet[ii + 1] = int(input())
-  ii += 1
+print(f"UDP target IP: {UDP_IP} and Port: {UDP_PORT} with message: {packet}")
 
-print(f"{packet}")
-
-sock = socket.socket(socket.AF_INET, # Internet
-                       socket.SOCK_DGRAM) # UDP
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
 sock.sendto(packet, (UDP_IP, UDP_PORT))
